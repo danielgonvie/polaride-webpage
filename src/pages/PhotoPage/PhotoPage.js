ea433e324e9007a38e7eb43400d793f7714385c5
danielgonvie <daniel.gonvie@gmail.com>
2020-07-26 01:03:50 +0200

    carousel working :D

--- src/pages/PhotoPage/PhotoPage.js
+++ src/pages/PhotoPage/PhotoPage.js
@@ -3,6 +3,9 @@ import "./PhotoPage.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 export default class PhotoPage extends Component {
 
   constructor(props) {
@@ -11,6 +14,27 @@ export default class PhotoPage extends Component {
   }
 
     render() {
+
+      const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
         return (
           <React.Fragment>
               <Navbar title={this.title}></Navbar>
@@ -19,7 +43,33 @@ export default class PhotoPage extends Component {
                 <img className="title-img photo-img" src="images/Photo.png" alt=""></img>
               </div>
               <div className="carrousell smartphone">
-
+                <Carousel
+                  additionalTransfrom={0}
+                  arrows
+                  autoPlaySpeed={3000}
+                  centerMode
+                  className="lol"
+                  containerClass="container"
+                  dotListClass=""
+                  draggable
+                  focusOnSelect={false}
+                  infinite
+                  itemClass="cool-item"
+                  keyBoardControl
+                  minimumTouchDrag={80}
+                  renderButtonGroupOutside={false}
+                  renderDotsOutside={false}
+                  responsive={responsive}
+                  showDots={false}
+                  sliderClass=""
+                  slidesToSlide={1}
+                  swipeable
+                >
+                  <div className="cool-div"><img className="marco  cool-photo" src="images/PolaridePhotoEdu-2.JPG" alt=""></img></div>
+                  <div className="cool-div"><img className="marco  cool-photo" src="images/PolaridePhotokiu-1.JPG" alt=""></img></div>
+                  <div className="cool-div"><img className="marco  cool-photo" src="images/PolaridePhotoKiu.JPG" alt=""></img></div>
+                  <div className="cool-div"><img className="marco  cool-photo" src="images/PolaridePhotoKiu-2.JPG" alt=""></img></div>
+                </Carousel>
               </div>
               <div className="great-container pc">
                 <div className="photo-container">
