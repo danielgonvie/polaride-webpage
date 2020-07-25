2409ac939b1b94e134ebc838e70725372b10652e
danielgonvie <daniel.gonvie@gmail.com>
2020-07-25 22:31:52 +0200

    maintenance

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -16,15 +16,27 @@ export default class HomePage extends Component {
         if(this.maintenance){
             return (
                 <React.Fragment>
-                    <div class="landing-background overflow-no">
-                    <div class="main-content">
-                        <img class="polaride-logo element" src="images/Polaride.png" alt=""></img>
-                        <p class="warning">¡Página en mantenimiento, disculpa las molestias!</p>
+                    <div className="landing-background overflow-no pc">
+                    <div className="main-content">
+                        <img className="polaride-logo element" src="images/Polaride.png" alt=""></img>
+                        <p className="warning">¡Página en mantenimiento, disculpa las molestias!</p>
                         <div>
-                            <a class="rrss-logo-instagram" href="https://www.instagram.com/polaridefilm/" ><img class="rrss-logo-instagram" src="images/Logo Instagram.png" alt=""></img></a>
-                            <a class="rrss-logo-linkedin" href="https://www.linkedin.com/in/paula-correa/" ><img class="rrss-logo-linkedin" src="images/LogoLinkedin.png" alt=""></img></a>
+                            <a className="rrss-logo-instagram" href="https://www.instagram.com/polaridefilm/" ><img className="rrss-logo-instagram" src="images/Logo Instagram.png" alt=""></img></a>
+                            <a className="rrss-logo-linkedin" href="https://www.linkedin.com/in/paula-correa/" ><img className="rrss-logo-linkedin" src="images/LogoLinkedin.png" alt=""></img></a>
                         </div>   
                       </div>
+                    </div>
+
+                    <div className="landing-maintenance smartphone">
+                        <div className="bg-polaride-maintenance">
+                            <img src="/images/Polaride.png" alt=""></img>
+                        </div>
+                        <p className="warning">¡Página en mantenimiento!</p>
+                        <p className="warning">Por favor, vuelve más tarde :)</p>
+                        <div className="emergency-logos">
+                            <a className="rrss-logo-instagram" href="https://www.instagram.com/polaridefilm/" ><img className="rrss-logo-instagram" src="images/Logo Instagram.png" alt=""></img></a>
+                            <a className="rrss-logo-linkedin" href="https://www.linkedin.com/in/paula-correa/" ><img className="rrss-logo-linkedin" src="images/LogoLinkedin.png" alt=""></img></a>
+                        </div>   
                     </div>
                 </React.Fragment>
             );
@@ -32,34 +44,34 @@ export default class HomePage extends Component {
         }else{
             return (
                 <React.Fragment>
-                  <div class="landing-background">
-                      <div class="main-content">
-                      <div className="nav-bar">
+                  <div className="landing-background">
+                      <div className="main-content">
+                      <div classNameName="nav-bar">
                           {this.title === "/" ? <NavbarOption option="/" selected></NavbarOption> : <NavbarOption option="/"></NavbarOption>}
                           {this.title === "photo" ? <NavbarOption option="photo" selected></NavbarOption> : <NavbarOption option="photo"></NavbarOption>}
                           {this.title === "video" ? <NavbarOption option="video" selected></NavbarOption> : <NavbarOption option="video"></NavbarOption>}
                           {this.title === "contact" ? <NavbarOption option="contact" selected></NavbarOption> : <NavbarOption option="contact"></NavbarOption>}
                       </div>
-                          <img class="polaride-logo element" src="images/Polaride.png" alt=""></img>
-                          <p class="mobile-text ">PHOTOGRAPHY & VIDEO</p>
+                          <img className="polaride-logo element" src="images/Polaride.png" alt=""></img>
+                          <p className="mobile-text ">PHOTOGRAPHY & VIDEO</p>
                       </div>
                   </div>
                   <Link to="photo">
-                      <div class="landing-photo">
-                          <div class="opacity-div"></div>
-                          <img class="title-img photo-land" src="images/Photography.png" alt=""></img>
+                      <div className="landing-photo">
+                          <div className="opacity-div"></div>
+                          <img className="title-img photo-land" src="images/Photography.png" alt=""></img>
                       </div>
                       </Link>
                       <Link to="video">
-                      <div class="landing-video">
-                          <div class="opacity-div"></div>
-                          <img class="title-img video-land" src="images/Films.png" alt=""></img>
+                      <div className="landing-video">
+                          <div className="opacity-div"></div>
+                          <img className="title-img video-land" src="images/Films.png" alt=""></img>
                       </div>
                       </Link>
                       <Link to="contact">
-                          <div class="landing-contact">
-                              <div class="opacity-div"></div>
-                              <img class="title-img contact-land" src="images/Contact.png" alt=""></img>
+                          <div className="landing-contact">
+                              <div className="opacity-div"></div>
+                              <img className="title-img contact-land" src="images/Contact.png" alt=""></img>
                           </div>
                       </Link>
                       <Footer></Footer>
